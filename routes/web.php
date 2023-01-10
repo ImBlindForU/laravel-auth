4c3e57a9ed5d63f7c9640078d85d1aeda005fcf2
ImBlindForU <samuele.zanchi10@gmail.com>
2023-01-10 16:29:00 +0100

    dashboard base

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/',[DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('admin.dashboard');
+
+require __DIR__.'/auth.php';
