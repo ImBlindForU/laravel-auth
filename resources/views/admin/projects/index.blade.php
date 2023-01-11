2bda1defdd5eb6d61161508728981ad58c432731
ImBlindForU <samuele.zanchi10@gmail.com>
2023-01-11 17:51:07 +0100

    destroy con message

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -33,13 +33,51 @@
                                     <a class="btn btn-warning" href="{{ route('admin.project.edit', $project->slug) }}">
                                         <i class="fa-solid fa-pen-to-square"></i>
                                     </a>
+                                    
+
+                                      <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#delete-project-{{ $project->id }}">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+
+                                    <!-- Modal della conferma prima della cancellazione -->
+                                    <div class="modal fade" id="delete-project-{{ $project->id }}" tabindex="-1"
+                                        aria-labelledby="delete-label-{{ $project->id }}" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h3 class="modal-title fs-5" id="delete-label-{{ $project->id }}">Vuoi
+                                                        cancellare {{ $project->title }}?</h3>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Annulla</button>
+                                                    <form action="{{ route('admin.project.destroy', $project->slug) }}"
+                                                        method="POST" class="d-inline-block">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button class="btn btn-danger" type="submit">
+                                                            Cancella
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+{{-- 
+
                                     <form action="{{ route('admin.project.destroy', $project->slug) }}" method="POST">
                                         @csrf
                                         @method('DELETE')
                                         <button class="btn btn-danger">
                                             <i class="fa-solid fa-trash"></i>
                                         </button>
-                                    </form>
+                                    </form> --}}
                                 </td>
                             </tr>
                         @endforeach
